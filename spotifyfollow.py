3b67b78d2b8f0bb601ab44847fa70f31f6f69fb7
baum1810 <72774679+baum1810@users.noreply.github.com>
2021-10-01 14:15:46 +0200

    Add files via upload

---
+++ spotifyfollow.py
@@ -0,0 +1,21 @@
+import requests
+import random
+
+
+
+username = input("username: ")
+with open("token.txt") as file:
+    while (line := file.readline().rstrip()):
+
+
+
+ 
+
+
+
+
+        headers = {
+    'authorization': f"{line}"
+    }
+        r = requests.put(f"https://api.spotify.com/v1/me/following?type=user&ids={username}", headers=headers)
+        print(r.status_code)    
