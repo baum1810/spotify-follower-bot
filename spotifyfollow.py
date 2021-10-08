c7f31bc2c63805673bb028a7d7fdbe1788b25277
baum1810 <72774679+baum1810@users.noreply.github.com>
2021-10-08 10:08:14 +0200

    Update spotifyfollow.py

--- spotifyfollow.py
+++ spotifyfollow.py
@@ -1,21 +1,11 @@
 import requests
 import random
 
-
-
 username = input("username: ")
 with open("token.txt") as file:
     while (line := file.readline().rstrip()):
-
-
-
- 
-
-
-
-
         headers = {
     'authorization': f"{line}"
     }
         r = requests.put(f"https://api.spotify.com/v1/me/following?type=user&ids={username}", headers=headers)
-        print(r.status_code)    
+        print(r.status_code) 
